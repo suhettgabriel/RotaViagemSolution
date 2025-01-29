83f8e18a09af00c080ddd2fd07ffbc2cf1876de6
suhettgabriel <suhettgabriel1983@gmail.com>
2025-01-29 10:15:05 -0300

    Criando primeira migration e ajustando configurações

---
+++ RotaViagem.Infrastructure/Data/RotaViagemContextFactory .cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+
+namespace RotaViagem.Infrastructure.Data
+{
+    public class RotaViagemContextFactory : IDesignTimeDbContextFactory<RotaViagemContext>
+    {
+        public RotaViagemContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<RotaViagemContext>();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(@"C:\Rota de Viagem\RotaViagem.Api\appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new RotaViagemContext(optionsBuilder.Options);
+        }
+    }
+}
